cb028d8c0628cb2f51063090c2bbaf859c218118
david <hoffi3089@gmail.com>
2024-08-17 21:34:03 -0700

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "../pages/home";
+import All from "../pages/total";
+import ProposalNewPage from "../pages/proposalNew";
+import ProposalSymbolPage from "../pages/proposalSymbol";
+import ProposalSymbolVotePage from "../pages/proposalSymbolVote";
+import { ReactApp } from "../components";
+import NotPage from "../pages/NotPage";
+import { config } from "../config/config";
+
+const { PUBLIC_URL } = config;
+
+const AppRoutes: FC = () => {
+  return (
+    <BrowserRouter basename={PUBLIC_URL}>
+      <Routes>
+        <Route element={<ReactApp />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<All />} />
+          <Route path="/proposal/new" element={<ProposalNewPage />} />
+          <Route path="/proposal/new/:protocol" element={<ProposalNewPage />} />
+          <Route path="/proposal/new/:protocol/:prsalType" element={<ProposalNewPage />} />
+          <Route path="/proposal/new/:protocol/:prsalType/:kpi" element={<ProposalNewPage />} />
+          <Route path="/proposal/new/:protocol/:prsalType/:kpi/:status" element={<ProposalNewPage />} />
+          <Route path="/proposal/:symbol" element={<ProposalSymbolPage />} />
+          <Route path="/proposal/:symbol/vote" element={<ProposalSymbolVotePage />} />
+        </Route>
+        <Route path="*" element={<NotPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export { AppRoutes };
